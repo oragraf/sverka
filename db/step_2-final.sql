5824f11d3beddc13ce20535797095959f46236cf
Andrey Glinsky <andrey.glinsky@fors.ru>
2015-11-03 09:43:47 +0300

    Подготовка дистрибутива. Параллель ограничена

--- db/step_2-final.sql
+++ db/step_2-final.sql
@@ -8,9 +8,9 @@ define script_desc='Скрипт перекидывает сиквенсы в актуальные значения. Запускае
 @@utl_head.sql
 @@it$$utl.sql
 
-alter session enable parallel dml   parallel 8;
-alter session enable parallel ddl   parallel 8;
-alter session enable parallel query parallel 4;
+alter session force parallel dml   parallel 4;
+alter session enable parallel ddl;
+alter session force parallel query parallel 4;
 
 declare
    cursor it$$dup_tables_c
